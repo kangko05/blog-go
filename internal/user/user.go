43ec8860bc91bbd9b738eaff6f788ce8add4d94d
kang@endv <kyungohkang126@gmail.com>
2025-09-15 13:02:28 +0900

    user module - not done

---
+++ internal/user/user.go
@@ -0,0 +1,45 @@
+package user
+
+import "golang.org/x/crypto/bcrypt"
+
+type UserRepository interface {
+	GetUser(username string) (*User, error)
+	SaveUser(user *User) error
+}
+
+type User struct {
+	Name           string
+	HashedPassword string
+}
+
+func New(username, password string) (*User, error) {
+	hashed, err := hashPassword(password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		Name:           username,
+		HashedPassword: hashed,
+	}, nil
+}
+
+func Validate(ur UserRepository, username, plainPassword string) error {
+	foundUser, err := ur.GetUser(username)
+	if err != nil {
+		return err
+	}
+
+	return bcrypt.CompareHashAndPassword([]byte(foundUser.HashedPassword), []byte(plainPassword))
+}
+
+// ===========================================================================
+
+func hashPassword(password string) (string, error) {
+	hashedB, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedB), nil
+}
