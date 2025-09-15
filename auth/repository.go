ce8afb4daefbc2819add2978619f22bfbb67dc13
kang@endv <kyungohkang126@gmail.com>
2025-09-15 10:54:16 +0900

    starting over...

--- auth/repository.go
+++
@@ -1,9 +0,0 @@
-package auth
-
-type Repository interface {
-	CheckUserExists(username string) (bool, error)
-	GetUser(username string) (*User, error)
-	SaveUser(user *User) error
-	SaveToken(token *Token) error
-	DeleteToken(tokenString string) error
-}
--- main.go
+++ main.go
@@ -1,37 +1,3 @@
 package main
 
-import (
-	"blog-go/auth"
-	"blog-go/database"
-	"blog-go/logger"
-	"blog-go/router"
-
-	"github.com/joho/godotenv"
-)
-
-func main() {
-	if err := godotenv.Load(); err != nil {
-		panic(err)
-	}
-
-	// init app
-	if err := database.Init("dev.db"); err != nil {
-		panic(err)
-	}
-	defer database.Close()
-
-	ar, err := database.NewAuthRepository()
-	if err != nil {
-		panic(err)
-	}
-
-	auth.Init(ar)
-
-	router.Init(logger.NewConsoleLogger())
-
-	//
-
-	if err := router.New().Run(":8000"); err != nil {
-		panic(err)
-	}
-}
+func main() {}
